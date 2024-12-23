b568354af2638c0afc5d59c12682b26d5a54048e
Ziyad-Qassem <Qassemziyad007@gmail.com>
2024-12-23 03:15:28 +0200

    xcode project created

---
+++ Uber_Clone/Uber_Clone/Uber_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_CloneApp.swift
+//  Uber_Clone
+//
+//  Created by Ziyad Qassem on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Uber_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
